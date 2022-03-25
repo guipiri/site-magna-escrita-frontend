9dbfd5afc0d772582adcc40988b5e655fb64de25
guipiri <gui.soliveiras@gmail.com>
2022-03-24 21:24:08 -0300

    Imagem redonda adicionada à página de contato

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,6 +3,7 @@ import GoToTop from "../gototop/GoToTop";
 import Form from "../layout/Form";
 import styles from "./Contact.module.css";
 import emailjs, { init } from "@emailjs/browser";
+import carousel2 from "../../img/carousel2.jpg";
 init("Q8esOQNBeIjtRDAeB");
 
 function Contact() {
@@ -13,7 +14,11 @@ function Contact() {
   }
   return (
     <div className={styles.contact}>
-      <Form handleSubmit={sendEmail} />
+      <img src={carousel2} alt="imagens do projeto" />
+      <div>
+        <h2>Ficou interessada ou ainda tem alguma dúvida? Vamos conversar!</h2>
+        <Form handleSubmit={sendEmail} />
+      </div>
       <GoToTop />
     </div>
   );
